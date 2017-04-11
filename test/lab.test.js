aebb4c6d9c2dedc5731ba1a21a089e1c4cde2bdc
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-11 15:10:36 -0700

    One passing asynch test with JSON

--- test/lab.test.js
+++ test/lab.test.js
@@ -5,8 +5,9 @@ const getObj = require('../lab');
 describe('get object', () => {
   
   it('reads a file', done => {
-    fs.readFile('./data/test.txt', (err, data) => {
-      assert.equal(data, 'test');
+    fs.readFile('./data/fluffy.json', (err, data) => {
+      const jsonData = JSON.parse(data);
+      assert.deepEqual(jsonData, {'name': 'fluffy'});
       done();
     });
     console.log('hello');
