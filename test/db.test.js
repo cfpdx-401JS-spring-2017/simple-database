a1f8c588a2d9ab0b14ae42ac18e847f3daa8d9e7
Ivy Patton <lilaivy@gmail.com>
2017-04-11 14:59:59 -0700

    first test passing

---
+++ test/db.test.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const assert = require('assert');
+
+describe('get object', () => {
+    it('reads a file', done => {
+        fs.readFile('./data/test.txt', (err, data) => {
+            assert.equal(data, 'test');
+            done();
+        });
+
+        console.log('hello, or something silly');
+    });
+});
