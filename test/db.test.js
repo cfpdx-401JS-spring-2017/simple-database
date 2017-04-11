73b3eaa6b5a4e81e626d0c664af50f0ef4a54dc5
nicky evers <livelyevers@gmail.com>
2017-04-11 16:29:32 -0700

    got three tests working with different types of data objects being retrieved.

--- test/db.test.js
+++ test/db.test.js
@@ -1,12 +1,37 @@
 const fs = require('fs');
 const assert = require('assert');
+const getObject = require('../lib/db');
 
-describe('get object', () => {
-  it('reads a file', done => {
-    fs.readFile('./data/cats/fluffy.json', (err, data) => {
-      const jsonData = JSON.parse(data);
-      assert.deepEqual(jsonData, {'name': 'fluffy'});
+describe('get object from database', () => {
+
+  it('gets an object given an id', done => {
+    getObject('./data', 'cats', 'f1de5', (err, data) => {
+      assert.deepEqual(data, {
+        'name': 'fluffy',
+        '_id': 'f1de5'
+      });
       done();
     });
   });
+
+  it('gets a cat object from database with different id', done => {
+    getObject('./data', 'cats', '7asn3', (err, data) => {
+      assert.deepEqual(data, {
+        'name': 'fred',
+        '_id': '7asn3'
+      });
+      done();
+    });
+  });
+  
+  it('gets a dog object from database', done => {
+    getObject('./data', 'dogs', 'ts21j', (err, data) => {
+      assert.deepEqual(data, {
+        'name': 'woof',
+        '_id': 'ts21j'
+      });
+      done();
+    });
+  });
+
 });
