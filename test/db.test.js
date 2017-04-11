71a3f9995e587b94a864f85dc25e5db97fb24b53
Morgan Fogarty <mofo37@gmail.com>
2017-04-11 15:11:31 -0700

    parsed data from json file

--- test/db.test.js
+++ test/db.test.js
@@ -3,8 +3,9 @@ const assert = require('assert');
 
 describe('get object', () => {
   it('reads a file', done => {
-    fs.readFile('./data/test.txt', (err, data) => {
-      assert.equal(data, 'test');
+    fs.readFile('./data/fluffy.json', (err, data) => {
+      const jsonData = JSON.parse(data);
+      assert.deepEqual(jsonData, {'name': 'fluffy'});
       done();
     });
     console.log('hello, or something');
