1f010af9d18718407ba921775e3423315d0ddade
Ivy Patton <lilaivy@gmail.com>
2017-04-11 15:17:56 -0700

    getting started on cat directory

--- test/db.test.js
+++ test/db.test.js
@@ -3,11 +3,16 @@ const assert = require('assert');
 
 describe('get object', () => {
     it('reads a file', done => {
-        fs.readFile('./data/test.txt', (err, data) => {
-            assert.equal(data, 'test');
+        fs.readFile('./data/cats/fluffy.json', (err, data) => {
+            //use deepequal to compare the guts of an object
+            //use json.parse to turn stringified object back into reg obj
+            const jsonData = JSON.parse(data);
+            assert.deepEqual(jsonData, {'name':'fluffy'});
             done();
         });
 
         console.log('hello, or something silly');
     });
 });
+
+
