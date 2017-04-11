871f8dd71e51881333f47043c58de0fdad27427a
Martin <martinconradpelayo@gmail.com>
2017-04-11 15:16:24 -0700

    more testing

---
+++ test/db.test.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const assert = require('assert');
+
+describe('get object', () => {
+    it('reads a file', done => {
+        fs.readFile('./data/cats/fluffy.json', (err, data) => {
+            const jsonData = JSON.parse(data);
+            assert.deepEqual(jsonData, { 'name': 'fluffy'});
+            done();
+        });
+    });
+
+});
