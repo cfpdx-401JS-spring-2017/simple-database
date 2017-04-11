ff24528d9469e4fa1397bfbc695f89d9d4d33353
Chris_Wallace <kawallis@icloud.com>
2017-04-11 15:58:02 -0700

    starting mob

--- test/db.test.js
+++ test/db.test.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 const assert = require('assert');
+const getObject = require('../lib/db');
 
 describe('get object', () => {
     it('reads a file', done => {
-        fs.readFile('./data/hood.json', (err, data) => {
-            const jsonData = JSON.parse(data);
-            assert.deepEqual(jsonData, {'name': 'street'});
-            done();
+        getObject('./data', 'cats', 'hood', (err, data) => {
+            assert.deepEqual(data, {'name': 'street', 'id': 'hood'});
         });
-        console.log('hello');
+        done();
     });
 
 });
