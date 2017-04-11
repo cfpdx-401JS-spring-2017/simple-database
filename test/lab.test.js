4242727ac7e38356d947ad1fba28ce518e231ed0
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-11 16:37:56 -0700

    Changes to file

--- test/lab.test.js
+++ test/lab.test.js
@@ -3,13 +3,9 @@ const assert = require('assert');
 const getObject = require('../lib/lab');
 
 describe('get object', () => {
-
   it('gets an object given an id', done => {
-    // fs.readFile('./data/cats/fluffy.json', (err, data) => {
-    //   const jsonData = JSON.parse(data);
-    //   assert.deepEqual(jsonData, {'name': 'fluffy'});
-    //   done();
     getObject('./data', 'cats', 'f1de5', (err, data) => {
+      console.log('THIS IS THE CALLBACK');
       assert.deepEqual(data, {
         '_id': 'f1de5',
         'name': 'fluffy'
