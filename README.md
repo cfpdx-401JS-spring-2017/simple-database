d0308b451acf67fff5927512948fdc076b840513
Martin <martinconradpelayo@gmail.com>
2017-04-11 17:19:29 -0700

    Started lab1

---
+++ README.md
@@ -0,0 +1,9 @@
+#Lab 1: Martin Pelayo
+
+`getObject(<data-dir>, <table>, <id>, callback)`
+
+Usage
+```
+getObject('./data', 'cats', f1de5, callback)
+
+```
--- test/db.test.js
+++ test/db.test.js
@@ -3,11 +3,16 @@ const assert = require('assert');
 const getObject = require('../lib/db');
 
 describe('get object', () => {
-    it('reads a file given an id', done => {
+    it('gets a cat given an id', done => {
         getObject('./data', 'cats', 'f1de5', (err, data) => {
             assert.deepEqual(data, {'name': 'fluffy', '_id': 'f1de5'});
             done();
         });
     });
-
+    it('returns null when cant find object by id', done => {
+        getObject('./data', 'cats', 'donesnotexist', (err, data) => {
+            assert.equal(data, null);
+            done();
+        });
+    });
 });
