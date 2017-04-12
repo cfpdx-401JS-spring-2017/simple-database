6aa556f141d5acb89a60049787737df736846314
Chris_Wallace <kawallis@icloud.com>
2017-04-11 17:19:41 -0700

    added changs

--- test/db.test.js
+++ test/db.test.js
@@ -6,8 +6,32 @@ describe('get object', () => {
     it('reads a file', done => {
         getObject('./data', 'cats', 'hood', (err, data) => {
             assert.deepEqual(data, {'name': 'street', 'id': 'hood'});
+            done();
+        });  
+    });
+    it('reads a file', done => {
+        getObject('./data', 'cats', 'nono', (err, data) => {
+            assert.deepEqual(data, {'name': 'billy', 'id': 'nono'});
+            done();
+        });  
+    });
+    it('reads a file', done => {
+        getObject('./data', 'dogs', 'hood', (err, data) => {
+            assert.deepEqual(data, {'name': 'street', 'id': 'hood'});
+            done();
+        });  
+    });
+    it('reads a file', done => {
+        getObject('./data', 'dogs', 'nono', (err, data) => {
+            assert.deepEqual(data, {'name': 'billy', 'id': 'nono'});
+            done();
+        });  
+    });
+    it('returns null when cant find object by id ', done => {
+        getObject('./data', 'cats', 'doesnotexist', (err, data) => {
+            assert.equal(data, null);
+            done();
         });
-        done();
     });
 
 });
