8f16a4ed27c4e4e2bd13362dde7904a7cb40b7a0
Martin <martinconradpelayo@gmail.com>
2017-04-12 15:56:12 -0700

    working

--- test/db.test.js
+++ test/db.test.js
@@ -5,39 +5,56 @@ const dbFactory = require('../lib/db-factory');
 const TEST_DIR = './data';
 const db = dbFactory(TEST_DIR);
 
-describe('db.get', () => {
-    it('gets a cat given an id', done => {
-        db.get('cats', 'f1de5', (err, data) => {
-            assert.deepEqual(data, {'name': 'fluffy', '_id': 'f1de5'});
-            done();
-        });
-    });
-    
-    it('gets a cat given an id', done => {
-        db.get('cats', 'otherCat', (err, data) => {
-            assert.deepEqual(data, {'name': 'Kitty', '_id': 'catID'});
-            done();
-        });
-    });
 
-    it('gets a dog given an id', done => {
-        db.get('dogs', 'woof', (err, data) => {
-            assert.deepEqual(data, {'name': 'sophie', '_id': 'dogID'});
-            done();
+describe('db', () => {
+    describe('db.get', () => {
+        it('gets a cat given an id', done => {
+            db.get('cats', 'f1de5', (err, data) => {
+                assert.deepEqual(data, {'name': 'fluffy', '_id': 'f1de5'});
+                done();
+            });
+        });
+        
+        it('gets a cat given an id', done => {
+            db.get('cats', 'otherCat', (err, data) => {
+                assert.deepEqual(data, {'name': 'Kitty', '_id': 'catID'});
+                done();
+            });
+        });
+
+        it('gets a dog given an id', done => {
+            db.get('dogs', 'woof', (err, data) => {
+                assert.deepEqual(data, {'name': 'sophie', '_id': 'dogID'});
+                done();
+            });
+        });
+
+        it('gets a dog given an id', done => {
+            db.get('dogs', 'bark', (err, data) => {
+                assert.deepEqual(data, {'name': 'doggo', '_id': 'pupID'});
+                done();
+            });
+        });
+
+        it('returns null when cant find object by id', done => {
+            db.get('cats', 'donesnotexist', (err, data) => {
+                assert.equal(data, null);
+                done();
+            });
         });
     });
+    describe('db.save', () => {
 
-    it('gets a dog given an id', done => {
-        db.get('dogs', 'bark', (err, data) => {
-            assert.deepEqual(data, {'name': 'doggo', '_id': 'pupID'});
-            done();
-        });
-    });
-
-    it('returns null when cant find object by id', done => {
-        db.get('cats', 'donesnotexist', (err, data) => {
-            assert.equal(data, null);
-            done();
+        it('saves data in new file and returns object with new id', (done) => {
+            const fluffy = {name: 'fluffy',
+                type: 'scotish fold'
+            };
+            db.save('cats', fluffy, (err, cat) => {
+                if(err) return console.log('ERROR', err);
+                assert.equal(cat.name, fluffy.name);
+                assert.ok(cat._id);
+                done();
+            });
         });
     });
 
