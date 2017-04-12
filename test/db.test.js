2f2ca5e412cfe81f3a54c53d9fd289415ced9425
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-12 15:05:38 -0700

    Refactored functions and began writing db.save (it does not work right now)

--- test/db.test.js
+++ test/db.test.js
@@ -1,40 +1,53 @@
 const assert = require('assert');
+const shortid = require('shortid');
 const SimpleDb = require('../lib/simple-db');
 
-const TEST_DIR = './data';
-const db = new SimpleDb(TEST_DIR);
+const ROOT_DIR = './data';
+const db = new SimpleDb(ROOT_DIR);
 
-describe('get object', () => {
-  it('gets a cat given an id', done => {
-    const id = 'f1de5';
-    db.get('cats', id, (err, cat) => {
-      assert.equal(cat._id, id);
-      assert.equal(cat.name, 'fluffy');
-      done();
-    });
-  });
+describe('db', () => {
 
-  it('gets a dog given an id', done => {
-    db.get('dogs', 'l33t0', (err, data) => {
-      assert.deepEqual(data, {
-        '_id': 'l33t0',
-        'name': 'bowwow'
+  describe('db.get', () => {
+
+    it('gets a cat given an id', done => {
+      const id = 'f1de5';
+      db.get('cats', id, (err, cat) => {
+        assert.equal(cat._id, id);
+        assert.equal(cat.name, 'fluffy');
+        done();
       });
-      done();
+    });
+
+    it('gets a dog given an id', done => {
+      const id = 'l33t0';
+      db.get('dogs', id, (err, dog) => {
+        assert.equal(dog._id, id);
+        assert.equal(dog.name, 'bowwow');
+        done();
+      });
+    });
+
+    it('returns null when can\'t find object by id', done => {
+      db.get('cats', 'doesnotexist', (err, data) => {
+        assert.equal(data, null);
+        done();
+      });
+    });
+  });
+  
+  describe('db.save', () => {
+    it('saves a cat', done => {
+      const id = shortid.generate();
+      const newCat = {
+        _id: id,
+        name: 'Tom'
+      };
+      db.save('cats', newCat, (err, cat) => {
+        //
+        assert.equal(cat, {});
+        done();
+      });
     });
   });
 
-  it('returns null when can\'t find object by id', done => {
-    db.get('cats', 'doesnotexist', (err, data) => {
-      assert.equal(data, null);
-      done();
-    });
-  });
-
-  // // it('saves', done => {
-  // //   const cat = {
-  // //     name: 'Tom',
-  // //     _id:
-  // //   }
-  // // })
 });
