5700bd4394cf0493c3b4a0eb0e37ae60bb2459f1
Ivy Patton <lilaivy@gmail.com>
2017-04-12 18:47:33 -0700

    still working on returning array

--- test/db.test.js
+++ test/db.test.js
@@ -1,6 +1,14 @@
 
 const assert = require('assert');
 const rimraf = require('rimraf');
+const testCat = {
+    name: 'testCat',
+    type: 'best'
+};
+const testDog = {
+    name: 'testDog',
+    type: 'bestDog'
+};
 const dbFactory = require('../lib/db-factory');
 
 
@@ -8,6 +16,22 @@ const TEST_DIR = './data';
 const db = dbFactory(TEST_DIR);
 
 describe('db', () => {
+    before((done) => {
+        db.save('cats', testCat, (err, data) => {
+            if (err) return done(err);
+            testCat._id = data._id;
+            done();
+        });
+    });
+
+    before((done) => {
+        db.save('dogs', testDog, (err, data) => {
+            if (err) return done(err);
+            testDog._id = data._id;
+            done();
+        });
+    });
+
     describe('db.get()', () => {
 
         it('returns null when can\'t find object by id', done => {
@@ -19,33 +43,32 @@ describe('db', () => {
         });
 
         it('gets a cat object given an id', done => {
-            db.get('cats', 'f1de5', (err, data) => {
-                assert.deepEqual(data, {
-                    'name': 'fluffy',
-                    '_id': 'f1de5'
-                });
+            db.get('cats', testCat._id, (err, data) => {
+                if (err) return done(err);
+                assert.equal(data.name, testCat.name);
+                assert.equal(data._id, testCat._id);
                 done();
             });
         });
-
-        it('gets a 2nd cat object given an id', done => {
-            db.get('cats', 'oxe34', (err, data) => {
-                assert.deepEqual(data, {
-                    'name': 'ada',
-                    '_id': 'oxe34'
-                });
-                done();
-            });
-        });
-        // it('gets a dog object given an id', done => {
-        //     db.get('dogs', '6ht7u', (err, data) => {
+//TODO: make 2nd Test Cat
+        // it('gets a 2nd cat object given an id', done => {
+        //     db.get('cats', 'oxe34', (err, data) => {
         //         assert.deepEqual(data, {
-        //             'name': 'beau',
-        //             '_id': '6ht7u'
+        //             'name': 'ada',
+        //             '_id': 'oxe34'
         //         });
         //         done();
         //     });
         // });
+        it('gets a dog object given an id', done => {
+            db.get('dogs', testDog._id, (err, data) => {
+                if (err) return done(err);
+                assert.equal(data.name, testDog.name);
+                assert.equal(data._id, testDog._id);
+                done();
+            });
+        });
+        //TODO: make a 2nd test dog
         // it('gets a 2nd dog object given an id', done => {
         //     db.get('dogs', 't33t0', (err, data) => {
         //         assert.deepEqual(data, {
@@ -55,6 +78,22 @@ describe('db', () => {
         //         done();
         //     });
         // });
+
+        describe('db.getAll', () => {
+            it ('returns an array of all objects from requested table', done => {
+
+                done();
+            });
+
+            //when writing tests, it's a good practice to start with the assert
+            it('checks that we returns empty array when no files in table', (done) => {
+                db.getAll('./bears', (err, bearsArray) => {
+                    //json.parse here
+                    assert.equal(bearsArray[0].name, 'baobao');
+                    done();
+                });
+            });
+        });
 
     });
 });
