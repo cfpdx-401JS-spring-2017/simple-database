dadc4a590420b94ac3f75046e2b043cd7d766968
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-21 14:35:11 -0700

    Promisified, 5 tests working, 4 remaining.

--- test/db.test.js
+++ test/db.test.js
@@ -22,53 +22,51 @@ describe('db', () => {
     });
   });
 
-  before(done => {
-    db.save('cats', fluffy, (err, data) => {
-      if (err) return done(err);
-      fluffy._id = data._id;
-      done();
+  before(() => {
+    return db.save('cats', fluffy)
+    .then(cat => {
+      fluffy._id = cat._id;
+    })
+    .catch(err => {
+      throw new Error(err);
     });
   });
 
-  before(done => {
-    db.save('cats', swifty, (err, data) => {
-      if (err) return done(err);
-      swifty._id = data._id;
-      done();
-    });
+  before(() => {
+    return db.save('cats', swifty)
+      .then(cat => {
+        swifty._id = cat._id;
+      });
   });
 
-  before(done => {
-    db.save('bears', gongfucha, (err, data) => {
-      if (err) return done(err);
-      gongfucha._id = data._id;
-      done();
-    });
+  before(() => {
+    return db.save('bears', gongfucha)
+      .then(data => {
+        gongfucha._id = data._id;
+      });
   });
 
   describe('db.save', () => {
 
-    it('saves a cat and returns file with new id', done => {
+    it('saves a cat and returns file with new id', () => {
       const maru = {
         name: 'maru',
         type: 'scottish fold'
       };
-      db.save('cats', maru, (err, cat) => {
-        if (err) return done(err);
+      return db.save('cats', maru)
+      .then(cat => {
         assert.equal(cat.name, maru.name);
         assert.ok(cat._id);
-        done();
       });
     });
 
-    it('creates directory if it doesn\'t exist', done => {
+    it('creates directory if it doesn\'t exist', () => {
       const baobao = {name: 'baobao', type: 'panda'};
-      db.save('bears', baobao, (err, bear) => {
-        if(err) return done(err);
-        db.get('bears', bear._id, (err, bear) => {
-          if (err) return done(err);
+      return db.save('bears', baobao)
+      .then(bear => {
+        db.get('bears', bear._id)
+        .then(bear => {
           assert.equal(bear.name, baobao.name);
-          done();
         });
       });
     });
@@ -76,81 +74,76 @@ describe('db', () => {
 
   describe('db.get', () => {
 
-    it('gets a cat given an id', done => {
+    it('gets a cat given an id', () => {
       const id = fluffy._id;
-      db.get('cats', id, (err, cat) => {
-        assert.equal(cat._id, fluffy._id);
-        assert.equal(cat.name, fluffy.name);
-        done();
-      });
+      return db.get('cats', id)
+        .then(cat => {
+          assert.equal(cat._id, fluffy._id);
+          assert.equal(cat.name, fluffy.name);
+        });
     });
 
-    it('returns null when can\'t find object by id', done => {
-      db.get('cats', 'doesnotexist', (err, data) => {
+    it('returns null when can\'t find object by id', () => {
+      return db.get('cats', 'doesnotexist')
+      .then(data => {
         assert.equal(data, null);
-        done();
       });
     });
   });
 
   describe('db.getAll', () => {
 
-    it('gets all objects in rootDir, returns array', done => {
-      db.getAll('cats', (err, cats) => {
-        if (err) return done(err);
-        assert.equal(cats.length, 3);
-        done();
-      });
+    it('gets all objects in rootDir, returns array', () => {
+      return db.getAll('cats')
+        .then(cats => {
+          assert.equal(cats.length, 3);
+        });
     });
 
   });
 
   describe('db.remove', () => {
 
-    it('removes an object from a directory', done => {
+    it('removes an object from a directory', () => {
       const id = swifty._id;
-      db.remove('cats', id, (err, data) => {
-        if (err) return done(err);
+      db.remove('cats', id)
+      .then(data => {
         assert.deepEqual(data, { removed: true });
-        done();
       });
     });
 
-    it('returns if object does not exist', done => {
+    it('returns if object does not exist', () => {
       const id = 'doesnotexist';
-      db.remove('cats', id, (err, data) => {
-        if (!err) return done(err);
+      return db.remove('cats', id)
+      .then(data => {
         assert.equal(data, { removed: false });
-        done();
       });
     });
   });
 
   describe('db.update', () => {
     
-    it('updates and returns an object', done => {
+    it('updates and returns an object', () => {
       const obj = gongfucha;
       gongfucha.type = 'red panda';
       const id = gongfucha._id;
-      db.update('bears', obj, (err, update) => { //eslint-disable-line
-        if (err) return done(err);
-        db.get('bears', id, (err, update) => {
-          if (err) return done(err);
+      return db.update('bears', obj)
+      .then(update => {
+        return db.get('bears', id)
+        .then(update => {
           assert.equal(update.type, 'red panda');
-          done();
         });
       });
     });
 
-    it('returns error if object does not exist', done => {
+    it('returns error if object does not exist', () => {
       const obj = {
         name: 'not real',
         type: 'imaginary' 
       };
-      db.update('bears', obj, err => {
-        if (!err) return done(err);
+      return db.update('bears', obj)
+      .then(err => {
         assert.ok(err, 'Error: Object does not exist.');
-        done();
       });
     });
   });
